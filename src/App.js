2645920098e6066bfa99e72ec1ae4648c0a74408
pai_jk <dlwnsrb05@publicai.co.kr>
2023-09-18 17:33:14 +0900

    Todo CRUD, Filter 주석 추가

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+// Todo의 체크 유무에 따라 filtering 하기 위한 객체
 const FILTER_MAP = {
   All: () => true,
   Active: (task) => !task.completed,
   Completed: (task) => task.completed,
 };
-
+// FILTER_MA의 key 값
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState("All");
 
-  //
+  // Todo의 체크 함수
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
@@ -31,19 +32,20 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  //C
+  // Create
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
-  // D
+  // Delete
   function deleteTask(id) {
     console.log(id);
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   }
 
+  // Update
   function editTask(id, newName) {
     const editedTaskList = tasks.map((task) => {
       // 이 할 일이 편집된 작업과 동일한 ID를 갖는 경우
@@ -56,7 +58,7 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  //task 데이터 정의
+  //Todo 컴포넌트 정의
   const taskList = tasks
     .filter(FILTER_MAP[filter])
     .map((task) => (
@@ -71,6 +73,7 @@ function App(props) {
       />
     ));
 
+  // filter button 컴포턴트 정의
   const filterList = FILTER_NAMES.map((name) => (
     <FilterButton
       key={name}
